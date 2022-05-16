453ca3edab0cb0cebbfef615e32dad9ff9e67550
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2022-05-16 06:11:07 -0500

    Remove unnecessary floating action button

--- app/src/main/java/com/giphy/ui/main/MainActivity.kt
+++ app/src/main/java/com/giphy/ui/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.giphy.ui.main
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
@@ -23,11 +21,5 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
